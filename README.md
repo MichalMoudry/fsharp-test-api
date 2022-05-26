b69e628d4e5205d6e2bbad0cb65ef60b88bb8ca8
Michal Moudrý <moudry.michal@live.com>
2022-05-26 21:26:24 +0200

    Changes to fsproj file + removed .sln file + changes to operations file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# fsharp-test-api
+# F# Test API
+Test project demonstrating a web API, that is built with F# and is using EF Core (with SQLite).
--- TestApi/Db/CoursesOperations.fs
+++ TestApi/Db/CoursesOperations.fs
@@ -2,14 +2,33 @@ namespace TestApi.Db
 
 open TestApi.Db.DataContext
 open TestApi.Models
+open System.Linq
 
+/// Class representing operations that relate to Course entity.
 type CoursesOperations(context: CoursesContext) =
     let dbContext = context
 
-    /// Function for adding a new course to db.
+    /// Method for adding a new course to db.
     member _.AddCourse(course: Course) =
         task {
             dbContext.Courses.Add(course) |> ignore
             dbContext.SaveChangesAsync() |> ignore
             return true
+        }
+
+    /// Method for obtaining a single Course entity.
+    member _.GetCourse(courseID: string) =
+        dbContext.Courses.SingleOrDefault(fun i -> i.ID.Equals(courseID))
+
+    /// Method for obtaining all the courses as an array.
+    member _.GetCourses() =
+        dbContext.Courses.ToArray()
+
+    /// <summary>Method for removing a course from db.</summary>
+    /// <param name="courseID">ID of a course.</param>
+    member this.DeleteCourse(courseID: string) =
+        task {
+            let course = this.GetCourse(courseID)
+            dbContext.Courses.Remove(course) |> ignore
+            dbContext.SaveChangesAsync() |> ignore
         }
