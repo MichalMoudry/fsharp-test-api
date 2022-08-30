0b6125b5a2c44102ff0510ded8dedfd365320fb4
Michal Moudrý <moudry.michal@live.com>
2022-08-30 18:10:36 +0200

    Changes to courses repository

--- TestApi/Db/Repositories/CoursesRepository.fs
+++ TestApi/Db/Repositories/CoursesRepository.fs
@@ -15,42 +15,9 @@ type CoursesRepository(context: DatabaseContext) =
             dbContext.Courses.AsNoTracking().ToArray()
         member _.GetById(id: Guid) =
             dbContext.Courses.Single(fun i -> i.Id.Equals(id))
-
-    /// Method for adding a new course to db.
-    member _.AddCourse(course: Course) =
-        task {
-            dbContext.Courses.Add(course) |> ignore
-            let! numberOfEntites = dbContext.SaveChangesAsync() |> Async.AwaitTask
-            if numberOfEntites > 0 then
-                return true
-            else
-                return false
-        }
-
-    /// Method for obtaining a single Course entity.
-    member this.GetCourse(id: Guid) =
-        (this :> IGenericRepository<Course>).GetById(id)
-
-    /// Method for obtaining all the courses as an array.
-    member _.GetCourses() =
-        dbContext.Courses.AsNoTracking().ToArray()
-
-    /// <summary>Method for removing a course from db.</summary>
-    /// <param name="courseID">ID of a course.</param>
-    member this.DeleteCourse(courseID: Guid) =
-        task {
-            let course = this.GetCourse(courseID)
-            dbContext.Courses.Remove(course) |> ignore
-            dbContext.SaveChangesAsync() |> ignore
-            return true
-        }
-
-    /// Method for updating an entity.
-    member this.UpdateCourse(newCourseData: Course) =
-        task {
-            let course = this.GetCourse(newCourseData.Id)
-            course.Name <- newCourseData.Name
-            course.DateUpdated <- DateTime.Now
-            dbContext.SaveChangesAsync() |> ignore
-            return true
-        }
+        member _.Add(entity: Course) =
+            true
+        member _.Update(entity: Course) =
+            true
+        member _.Delete(id: Guid) =
+            true
--- TestApi/Db/Repositories/Interfaces/IGenericRepository.fs
+++ TestApi/Db/Repositories/Interfaces/IGenericRepository.fs
@@ -2,18 +2,30 @@ namespace TestApi.Db.Repositories.Interfaces
 
 open System
 
+/// Interface for a generic repository.
 type IGenericRepository<'T> =
+    /// Method for obtaining all the entities as an array.
     abstract member GetAll: unit -> array<'T>
+
     /// Method for obtaining an entity by a Guid.
     abstract member GetById:
         id: Guid
         -> 'T
+
+    /// <summary>Method for adding a new entity to db.</summary>
+    /// <param name="entity">A new entity.</param>
     abstract member Add:
         entity: 'T
         -> bool
+
+    /// <summary>Method for updating an entity in db.</summary>
+    /// <param name="entity">Entity with new data.</param>
     abstract member Update:
         entity: 'T
         -> bool
+
+    /// <summary>Method for removing an entity from db.</summary>
+    /// <param name="id">ID of an entity.</param>
     abstract member Delete:
         id: Guid
         -> bool
